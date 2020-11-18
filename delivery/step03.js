7bd100163de306f3f133fd6281f3cb2419822959
leavan <leavan.vicente@epitech.eu>
2020-11-18 09:31:00 +0100

    -

---
+++ delivery/step03.js
@@ -0,0 +1,16 @@
+$(function () {
+    $(".grosseDescription").hide();
+    $("#GD1").click(function () {
+        $("#grosseDescription1").toggle();
+
+    });
+    $("#GD2").click(function () {
+        $("#grosseDescription2").toggle();
+
+    });
+    $("#GD3").click(function () {
+        $("#grosseDescription3").toggle();
+
+    });
+
+});
